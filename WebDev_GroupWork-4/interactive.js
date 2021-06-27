6986bdf78b6c6b7290f5826d6d90b3320d8a2bff
Larry <larrybe.dev@gmail.com>
2021-06-27 01:34:40 -0700

    Added video of roller coaster and fireworks show

--- WebDev_GroupWork-4/interactive.js
+++ WebDev_GroupWork-4/interactive.js
@@ -1,4 +1,4 @@
-function onReady(){
+function covidToggle(){
     let toggle = document.querySelector("#covid-info-expand");
     toggle.addEventListener("click", function(e){
         toggleCovidInfo();
@@ -17,6 +17,72 @@ function toggleCovidInfo(){
     }
 }
 
-document.addEventListener("DOMContentLoaded", onReady)
+function getVideos(){
+    let vids = [
+        document.querySelector("#wildfire-show video"),
+        document.querySelector("#drogon video")
+    ];
+    return vids;
+}
+
+function playInFrameVideo() {
+    let vids = getVideos();
+    document.addEventListener("scroll", function () {
+        for (var i = 0; i < vids.length; i++) {
+            let playCount = parseInt(vids[i].getAttribute("data-play-count"));
+            let maxReplays = parseInt(vids[i].getAttribute("data-max-replays"));
+            if (videoInFrame(vids[i])) {
+                if (playCount < maxReplays) {
+                vids[i].play();
+                }
+            } else {
+                vids[i].load();
+                vids[i].setAttribute("data-play-count", 0);
+            }
+        }
+    })
+}
+
+function videoInFrame(vid){
+    let v_bounds = vid.getBoundingClientRect();
+    if (window.innerHeight - v_bounds.top > 200 &&
+        v_bounds.top + v_bounds.height > 100){
+            return true;
+    }
+    return false;
+}
+
+function videoManager(){
+    let vids = getVideos();
+    for (var i = 0; i < vids.length; i++) {
+        vids[i].addEventListener("ended", function(e){
+            let playCount = parseInt(e.target.getAttribute("data-play-count"));
+            let maxReplays = parseInt(e.target.getAttribute("data-max-replays"))
+            e.target.setAttribute("data-play-count", ++playCount);
+            if (playCount < maxReplays) {
+                e.target.play();
+            } else {
+                e.target.load();
+            }
+        });
+
+        vids[i].addEventListener("click", function(e) {
+            let maxReplays = parseInt(e.target.getAttribute("data-max-replays"));
+            if (e.target.paused) {
+                e.target.play();
+                e.target.setAttribute("data-play-count", 0);
+            } else {
+                e.target.load();
+                e.target.setAttribute("data-play-count", maxReplays);
+            }
+        })
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    covidToggle();
+    playInFrameVideo();
+    videoManager();
+})
 
 
